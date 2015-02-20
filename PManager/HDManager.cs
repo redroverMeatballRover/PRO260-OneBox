947ba16c0cfeb13d62b54cdb71df4399a50a01cd
markus102994 <mjournigan@student.neumont.edu>
2015-02-20 15:26:50 -0700

    HDManager, PManager, and Indexer

---
+++ PManager/HDManager.cs
@@ -0,0 +1,91 @@
+﻿using DiscUtils.Vhd;
+using DiscUtils;
+using DiscUtils.Partitions;
+using DiscUtils.Ntfs;
+using DiscUtils.Fat;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PManager
+{
+    public class HDManager
+    {
+        /// <summary>
+        /// This method creates a .vhd hard drive formatted in FAT.
+        /// </summary>
+        /// <param name="diskSize">The size of the disk in bytes.</param>
+        /// <param name="location">Defines the physical location and file name. .vhd extension required.</param>
+        /// <param name="dynamic">Determines whether or not the disk will be of fixed size or dynamic size.</param>
+        /// <returns>A bool determining if the method succeeded.</returns>
+        public bool CreateFatDrive(long diskSize, string location, bool dynamic)
+        {
+            try {
+                using (Stream vhdStream = File.Create(@location))
+                {
+                    Disk disk = null;
+
+                    if (dynamic)
+                    {
+                        disk = Disk.InitializeDynamic(vhdStream, Ownership.None, diskSize);
+                    }
+                    else
+                    {
+                        disk = Disk.InitializeFixed(vhdStream, Ownership.None, diskSize);
+                    }
+
+                    BiosPartitionTable.Initialize(disk, WellKnownPartitionType.WindowsFat);
+                    FatFileSystem.FormatPartition(disk, 0, null);
+
+                    return true;
+                }
+            } 
+            catch (IOException e) 
+            {
+                Console.WriteLine(e.StackTrace);
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// This method creates a .vhd hard drive formatted in NTFS.
+        /// </summary>
+        /// <param name="diskSize">The size of the disk in bytes.</param>
+        /// <param name="location">Defines the physical location and file name. .vhd extension required.</param>
+        /// <param name="label">The label to be given to the disk.</param>
+        /// <param name="dynamic">Determines whether or not the disk will be of fixed size or dynamic size.</param>
+        /// <returns>A bool determining if the method succeeded.</returns>
+        public bool CreateNtfsDrive(long diskSize, string location, string label, bool dynamic)
+        {
+            try
+            {
+                using(Stream vhdStream = File.Create(@location))
+                {
+                    Disk disk = null;
+
+                    if(dynamic)
+                    {
+                        disk = Disk.InitializeDynamic(vhdStream, Ownership.None, diskSize);
+                    }
+                    else
+                    {
+                        disk = Disk.InitializeFixed(vhdStream, Ownership.None, diskSize);
+                    }
+
+                    BiosPartitionTable.Initialize(disk, WellKnownPartitionType.WindowsNtfs);
+                    NtfsFileSystem.Format(new VolumeManager(disk).GetLogicalVolumes()[0], label);
+
+                    return true;
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.StackTrace);
+                return false;
+            }
+        }
+    }
+}
---
+++ PManager/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace PManager
+{
+    public class PasswordManager
+    {
+        private string salt = "breezytelephone";
+
+        private string GetMD5Hash(string passwd)
+        {
+            MD5 md5 = MD5CryptoServiceProvider.Create();
+            byte[] inputBytes = System.Text.ASCIIEncoding.ASCII.GetBytes(passwd);
+            byte[] hash = md5.ComputeHash(inputBytes);
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+            return sb.ToString().ToLower();
+        }
+
+        private string GetMD5Hash(string password, bool salt, string value)
+        {
+            if (salt)
+            {
+                return GetMD5Hash(value + password);
+            }
+            else
+            {
+                return GetMD5Hash(password);
+            }
+        }
+
+        public static void Main(String[] args)
+        {
+            PasswordManager pm = new PasswordManager();
+            Console.WriteLine(pm.GetMD5Hash("String", true, pm.salt));
+
+            HDManager hdmanager = new HDManager();
+
+            int userid = (new Random()).Next();
+
+            string location = "C:\\TEMP\\";
+            location += userid + ".vhd";
+
+            long diskSize = (long)Math.Pow(1024, 3);
+
+            Console.WriteLine(hdmanager.CreateNtfsDrive(diskSize, location, "Disk", true));
+        }
+
+    }
+}
